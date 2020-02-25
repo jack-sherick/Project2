74e0f667dddf94daecb9c958416ca41b608a2727
jack-sherick <54997111+jack-sherick@users.noreply.github.com>
2020-02-25 10:52:11 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,44 @@
+//create render
 let renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-let worldWidth = 256, worldDepth = 256,
-	worldHalfWidth = worldWidth / 2, worldHalfDepth = worldDepth / 2;
-
-let camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 20000 );
-
+//create scene, clock, camera
 let scene = new THREE.Scene();
 scene.background = new THREE.Color(0xbfd1e5);
 
+let camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 20000 );
+camera.position.z = 5;
+camera.position.y = 1
+
 let clock = new THREE.Clock();
 
-let controls = new FirstPersonControls(camera, renderer.domElement);
-controls.movementSpeed = 1000;
-controls.lookSpeed = 0.1;
+//function for rendering
+function render() {
+	renderer.render(scene, camera);
+	
+}
+render();
+
+//fp controls
+let controls = new THREE.PointerLockControls(camera, document.body);
+controls.isLocked = true;
+scene.add(controls.getObject())
+
+//create cube
+var geometry = new THREE.BoxGeometry();
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+//floor
+
+
+//animation loop
+function cycle() {
+	requestAnimationFrame(cycle);
+	render();
+	
+	
+}
+requestAnimationFrame(cycle);
